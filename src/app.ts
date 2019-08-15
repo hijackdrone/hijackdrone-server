453a5d3b8231cae05332ed33f4488acc4254ec69
lsb918 <lsb918@uos.ac.kr>
2019-08-15 19:30:32 +0900

    some stuffs left

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,13 @@ import express from 'express';
 import * as http from 'http';
 import io from 'socket.io';
 
-type Controller = {
-	id: string,
-};
-type Drone = {
+type User = {
 	id: string,
 };
 type Room = {
 	name: string,
-	controllers?: Controller[],
-	drones?: Drone[],
-	count: number,
+	controllers?: User[],
+	drones?: User[],
 };
 type Result = {
 	status: boolean,
@@ -26,78 +22,183 @@ const Socket = io(server);
 const port = 4000;
 server.listen(port)
 
-app.get('/',(req,res)=>{
-  res.end('HIJACK DRONE\'s API server.');
+app.get('/', (req, res) => {
+	res.end('HIJACK DRONE\'s API server.');
 });
 
 const MAX_CONTROLLER = 2;
 const MAX_DRONE = 1;
 
-let Rooms: Room[]=[];
-Socket.on('connection', (socket)=>{
-	const id=socket.id;
+let Rooms: Room[] = [];
+
+Socket.on('connection', (socket) => {
+	const id = socket.id;
 	log(`user ${id} connected.`);
 
-	socket.on('disconnect', (socket)=>{
+	socket.on('disconnect', (socket) => {
+		exitUser(id);
 		log(`user ${id} disconnected.`);
+		/**
+		 *  send crdis or drdis, -> just send 'wait' ?
+		 * 
+		 * */
 	});
 
-	socket.on('find room',value => {
+	socket.on('find room', value => {
 		const roomName = value[0];
 		const roll = value[1];
-		if(roomName.length===0){
-			socket.emit('rejected room','room name must be longer than 0')
+		if (roomName.length === 0) {
+			socket.emit('rejected room', 'Name of \'Room\' must be longer than 0')
 			return;
 		}
 		const idx = findRoom(roomName);
-		if(idx<0){
+		if (idx < 0) {
 			// non existed room -> crete room
-		}else {
-			// room exists -> check available
-			const check=available(idx,roll);
-			if(check.status){
-				// room available for roll
+			let room : Room={
+				name: roomName,
+				controllers: [],
+				drones: [],
+			};
+			if(roll === 'c'){
+				room.controllers=[{id: id}];
 			} else {
-				socket.emit('rejected room',check.msg);
+				room.drones=[{id: id}];
+			}
+			Rooms.push(room);
+			/**
+			 * send 'found room' to socket id user.
+			 * 
+			 * 
+			 */
+		} else {
+			// room exists -> check available
+			const check = available(idx, roll);
+			if (check.status) {
+				addUser(idx, id, roll);
+				/** check room's status
+				 * & if connected -> send 'connected' to roomName
+				 * 
+				 * 
+				 * 
+				 * */ 
+			} else {
+				socket.emit('rejected room', check.msg);
 				log(`rejected room, to access ${JSON.stringify(Rooms[idx])}`);
 			}
+
 		}
 	})
 });
 
 function findRoom(name: string): number {
-	return Rooms.findIndex(e=>{e.name===name})
+	return Rooms.findIndex(e => { e.name === name })
 }
 
 function available(roomIdx: number, roll: string): Result {
 	const room = Rooms[roomIdx];
-	let res: Result={status: false, msg: ''};
-	switch(roll){
+	let res: Result = { status: false, msg: ''};
+	switch (roll) {
 		case 'c':
-			if(!room.controllers){
-				res.status=true;
+			if (!room.controllers) {
+				res.status = true;
 			} else {
-				res.status=(room.controllers).length < MAX_CONTROLLER;
-				res.msg=res.status?'' :'max controller';
+				res.status = (room.controllers).length < MAX_CONTROLLER;
+				res.msg = res.status ? '' : 'max controller';
 			}
 			break;
 		case 'd':
-			if(!room.drones){
-				res.status=true;
+			if (!room.drones) {
+				res.status = true;
 			} else {
-				res.status=(room.drones).length < MAX_DRONE;
-				res.msg=res.status?'' :'max drone';
+				res.status = (room.drones).length < MAX_DRONE;
+				res.msg = res.status ? '' : 'max drone';
 			}
 			break;
 		default:
-			res.status=false;
-			res.msg=res.status?'' :'hack';
+			res.status = false;
+			res.msg = res.status ? '' : 'hack';
 			break;
 	}
 	return res;
 }
+function addUser(idx: number, id: string, roll: string): void {
+	const Room = Rooms[idx];
+	switch (roll) {
+		case 'c':
+			if (Room.controllers) {
+				Room.controllers.push({ id: id });
+			} else {
+				Room.controllers = [{ id: id }];
+			}
+			break;
+		case 'd':
+			if (Room.drones) {
+				Room.drones.push({ id: id });
+			} else {
+				Room.drones = [{ id: id }];
+			}
+			break;
+		default:
+			break;
+	}
+}
+function exitUser(id: string, roomName?: string, roll?: string): void {
+	let idx: number;
+	if (roomName && roll) {
+		idx = findRoom(roomName);
+		switch (roll) {
+			case 'c':
+				const controllers = Rooms[idx].controllers!.filter(e => e.id !== id);
+				Rooms[idx].controllers = controllers;
+				break;
+			case 'd':
+				const drones = Rooms[idx].drones!.filter(e => e.id !== id);
+				Rooms[idx].drones = drones;
+				break;
+			default:
+				break;
+		}
+	} else {
+		// find...
+		let res: boolean = false;
+		idx = Rooms.findIndex(room => {
+			if (room.controllers) {
+				res = (room.controllers.filter(user => user.id === id)).length > 0;
+				if (res) return true;
+			}
+			if (room.drones) {
+				res = (room.drones.filter(user => user.id === id)).length > 0;
+				if (res) return true;
+			}
+			return false;
+		});
+	}
+	deleteVacantRoom(idx);
 
-function log(msg: string): void{
+}
+function deleteVacantRoom(idx: number): void {
+	if (idx < 0) {
+		const ctrs = Rooms[idx].controllers;
+		const drs = Rooms[idx].drones;
+		let res: boolean = false;
+		if (ctrs && drs) {
+			if (ctrs.length === 0 && drs.length === 0) {
+				// remove Rooms[idx];
+				res = true;
+			}
+		} else if (ctrs && ctrs.length === 0) {
+			res = true;
+		} else if (drs && drs.length === 0) {
+			res = true;
+		}
+		if (res) {
+			// dangerous.. if some rooms are deleting simultaneously
+			const roomName = Rooms[idx].name;
+			Rooms = Rooms.filter(e => e.name !== roomName);
+		}
+	}
+}
+function log(msg: string): void {
 	const timestamp = (new Date()).toISOString();
 	console.log(timestamp, msg);
 }
---
+++ src/editing.js
@@ -0,0 +1,145 @@
+let room=[];
+// 최후의 수단.
+// let cleanRoom=setInterval(()=>{
+//     const len_prev=room.length;
+//     room=room.filter(e=>{
+//         return e.drone[0] || e.control[0]
+//     });
+//     const len_next=room.length;
+//     console.log(len_prev-len_next,'room(s) cleaned.');
+// },60000);
+
+io.on('connection', (socket)=>{
+    const id=socket.id;
+    console.log(`user ${id} connected`);
+    socket.on('greeting',(value)=>{
+        console.log(value,id);
+    });
+    socket.on('disconnect',()=>{
+        console.log(`user ${id} disconnected`);
+        if(room.length>0){
+            const idx=room.findIndex(e=> e.drone[1]===id || e.control[1]===id)
+            if(idx>=0){
+                if(room[idx].drone[1]===id){
+                    room[idx].drone=[false,''];
+                    socket.to(room[idx].pw).emit('drdis');
+                } 
+                else{
+                    room[idx].control=[false,''];
+                    socket.to(room[idx].pw).emit('crdis');
+                }
+                if(!room[idx].control && !room[idx].drone) room.splice(idx,1);
+            }
+        }
+        console.log('room :',room)
+    });
+
+    // 2 joining room
+    socket.on('find room', value=>{
+        pw=value[0];
+        type=value[1];
+        if(pw.length===0){
+            socket.emit('rejected room','room name must be longer than 0')
+            return;
+        }
+        const idx=room.findIndex(e=>e.pw === pw);
+        const roomAvailable = checkRoom(idx,pw,type,socket.id); //[ true|false, errorMessage ]
+        console.log(roomAvailable);
+
+        if(roomAvailable[0]){
+            socket.emit('found room', pw);
+            console.log('found room');
+            socket.join(pw);
+            if(roomAvailable[1]===true){
+                socket.emit('connected');
+                socket.to(pw).emit('connected');
+                socket.broadcast.emit('connected'); // need to be fixed
+            }
+        }else{
+            socket.emit('rejected room',roomAvailable[1]);
+            console.log('rejected room',roomAvailable[1]);
+        }
+    });
+    socket.on('leave room',value=>{
+        pw=value[0];
+        type=value[1];
+        if(pw!==''){
+            const idx=room.findIndex(e=>e.pw === pw);
+            if(idx<0) socket.emit('err', 'room not existing');
+            else{
+                socket.leave(pw);
+                if(type==='c'){
+                    if(room[idx].control[1] === socket.id){
+                        room[idx].control = room[idx].control.slice(2,4);
+                        
+                    }else{
+                        room[idx].control = room[idx].control.slice(0,2);
+                    }
+                    room[idx].control=[false,''];
+                    socket.to(room[idx].pw).emit('crdis');
+                }
+                else {
+                    room[idx].drone=[false,''];
+                    socket.to(room[idx].pw).emit('drdis');
+                }
+                if(!room[idx].control[0] && !room[idx].drone[0]) room.splice(idx,1);
+                console.log('room :',room);
+            }
+        }
+    });
+    // 3 controll with drone & controller
+    // maybe many room will be stucked here... considering 'move' moves to line 30
+    socket.on('move',value=>{ 
+        const pw=value[0];
+        const data=value[1];
+        // console.log(pw,data);
+        socket.to(pw).emit('accept move',data);
+    });
+    socket.on('debug',value=>{
+	    console.log('from debug ::', value);
+    });
+})
+
+const checkRoom=(idx,pw,type,socketID)=>{
+    // type = 'd' or 'c'
+    if(room[idx]){ //room exist?
+        // console.log('room exist');
+        if(!room[idx].drone[0] && !room[idx].control[1]){ //false & false room
+            if(type === 'd' ) room[idx].drone=[true,socketID];
+            else room[idx].control=[true,socketID];
+            return [true, 'false && false room error solved.']
+        }
+        // 뭔가 있다.
+        if(room[idx].control.length === 4 && type==='c'){ // control 방 다참
+            return [false, `${type} already exists.`];
+        } else if(room[idx].control.length === 2 && type==='c'){ // control 방 들어가자.
+            try{
+                room[idx].control.push(true,socketID);
+            } catch(e) { // control not existed
+                room[idx].control=[true,socketID];
+            }
+            if(room[idx].drone[0]) return [true, true];
+            else return [false, 'waiting drone.'];
+        } else if(!room[idx].drone[0] && type==='d'){ // drone 방 들어가자.
+            room[idx].drone=[true,socketID];
+            if(room[idx].control.length === 4) return [true, true]; // 2:1 connection fin
+            else return [false, 'waiting control'];
+        } else { // unknown error
+            const roomToJson=JSON.stringify(room);
+            return [false, `unknown error, room : ${roomToJson}`];
+        }
+    }else{
+        // console.log('room doesn\'t exist');
+        if(type==='d'){
+            room.push({
+                pw, drone: [true, socketID], control: [false,'']
+            });
+        }
+        else if(type==='c'){
+            room.push({
+                pw, drone: [false,''], control: [true, socketID]
+            });
+        }
+        return [true,`made room ${pw}`];
+    }
+}
